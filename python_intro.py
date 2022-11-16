68d12f1aa1968dd51bc70277a27ec6839ffa7ac1
sofigatomagico <sofia.gmagk@gmail.com>
2022-11-16 13:50:20 -0600

    Mi aplicación Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,16 @@
+def hola(nombre):
+    print("Hola "+ nombre + "!")
+
+mascotas = ["Momo","Milo","Tofu","Henri"]
+for nombre in mascotas:
+    hola(nombre)
+    print("Siguiente mascota")
+
+for i in range (1,6):
+    print(i)  
+
+
+
+
+
+
